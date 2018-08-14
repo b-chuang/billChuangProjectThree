aa0d2389d1065b29af3cd11646565c642e382464
Bill <skeche@gmail.com>
2018-08-13 20:45:08 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,70 @@
+const gcs = {
+    eyes: [
+        {
+        /*  response: '4. Spontaneous', */
+        spontaneous: 4,
+        speech:3,
+        pain:2,
+        no response:1
+
+        },
+
+        {
+        spontaneous: 4,
+        speech: 3,
+        pain: 2,
+        no response: 1
+        },
+     /*    {
+            reponse:'3. To speech',
+
+
+        },
+        {
+            reponse: '2. To Pain',
+        },
+        {
+            reponse: '1. no response',
+        } */
+    ],
+
+    verbal: [
+        {
+            response: '5. oriented to time space',
+        },
+        {
+            response: '4. confused',
+        },
+        {
+            response: '3. inappropriate words',
+        },
+        {
+            response: '2. incomprehensible sounds',
+        },
+        {
+            response: '1. no response',
+        }
+    ],
+
+    motor: [
+        {
+            response:'6. obeys command',
+        },
+        {
+            response: '5. localises pain',
+        },
+        {
+            response: '4. flex to withdraw from pain',
+        },
+        {
+            response: '3. abnormal flexion',
+        },
+        {
+            response: '2. abnormal extension',
+        },
+        {
+            response: '1. no response',
+        }
+    ]
+};
+$(function () {
