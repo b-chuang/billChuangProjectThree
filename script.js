d0d2bab5240f7774310771500649e1d863c406d6
Bill <skeche@gmail.com>
2018-08-16 10:19:28 -0400

    added if else

--- script.js
+++ script.js
@@ -1,79 +1,3 @@
-//two ways of doing it
-//get the input in each behaviour
-
-
-
-/* const gcs = {
-    eyes: [
-        {
-        // response: '4. Spontaneous',
-        //spontaneous: 4,
-        //speech:3,
-        //pain:2,
-        //noresponse:1
-
-        },
-
-       {
-        spontaneous: 4,
-        speech: 3,
-        pain: 2,
-        noResponse: 1
-        },
-        
-        {
-            reponse:'3. To speech',
-        },
-        {
-            reponse: '2. To Pain',
-        },
-        {
-            reponse: '1. no response',
-        } 
-    ],
-
-    verbal: [
-        {
-            response: '5. oriented to time space',
-        },
-        {
-            response: '4. confused',
-        },
-        {
-            response: '3. inappropriate words',
-        },
-        {
-            response: '2. incomprehensible sounds',
-        },
-        {
-            response: '1. no response',
-        }
-    ],
-
-    motor: [
-        {
-            response:'6. obeys command',
-        },
-        {
-            response: '5. localises pain',
-        },
-        {
-            response: '4. flex to withdraw from pain',
-        },
-        {
-            response: '3. abnormal flexion',
-        },
-        {
-            response: '2. abnormal extension',
-        },
-        {
-            response: '1. no response',
-        }
-    ]
-}
- */
-
-
 const userData = {
     eye:'', 
     verb:'',
@@ -81,32 +5,79 @@ const userData = {
     total: ''
 }
 
-const total = (userData => {
-
+const total = (userData) => {
+    // console.log(userData)
     userData.eye = parseInt(userData.eye);
     userData.verb = parseInt(userData.verb);
     userData.motor = parseInt(userData.motor);
+    // console.log(userData);
+    // userData.total = userData.eye + userData.verb + userData.motor;
+    // return sum;
+    printToPage(userData);
 
-    sum = userData.eye + userData.verb + userData.motor;
-    return sum;
-});
+};
 
 
-//hidden info is hidden
-//toggle the hidden info when a number is reached
+const printToPage = (sum) => {
+    console.log(sum); //if statement.
+    if (userData.total >= 3 && userData.total <= 8 ) {
+        $('#result').html(`<h2> the patient is in high level risk </h2>`)
+        
+        //need to change .text to html for H2 tags, wont work in just text
+        console.log(`user data is between 3-8`);
+        
+        
+    } else if (userData.total > 8 && userData.total <= 12 ) {
+        $('#result').html(`<h2>the patient is in medium level risk </h2>`)
+
+        console.log(`user data is between 8-12`);
+
+        
+    } else if (userData.total > 12 && userData.total <= 15) {
+        $('#result').html(`<h2>the patient is in low level risk</h2>`)
+
+        console.log(`user data is between 12-15`);
+
+}   
+}
 
 
 $('form').on('submit', function(event){
     event.preventDefault();
-    //console.log('hello');
-    userData.eye = $('input[name=eye-response]:checked').val(); //names but changing value into checked items
+    userData.eye = $('input[name=eye-response]:checked').val(); 
     userData.verb = $('input[name=verb-response]:checked').val();
     userData.motor = $('input[name=motor-response]:checked').val();
+
+    userData.total = parseInt(userData.eye) + parseInt(userData.verb) + parseInt(userData.motor);
     
-    userData.total = total(userData); 
+    total(userData); 
     
-    console.log(total(userData));
-    console.log(userData)
+    // console.log(total(userData));
+    // console.log(userData)
     
-    $('.results').text(userData.total)
+
+
+
+    $('.results').html(`<h2 class="resultTotal"> ${userData.total} </h2>`)
+
+    
+
+   /*  $('form').trigger("reset"); */
+
 });
+
+
+
+
+//correct organisation
+//refresh clear
+
+
+
+
+//add in 3 hidden message that will pop up with corresponding value
+
+//make an if statement comparing userData.total to riskValue
+
+
+
